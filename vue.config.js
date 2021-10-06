406dcf0c567c55af9bd33e7da52e1913c62599fc
磊哥 <493114008@qq.com>
2021-10-06 12:25:36 +0800

    icon

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,21 @@
+
 module.exports = {
-  lintOnSave: false,
-  devServer:{
-    https:true
-  }
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/vue-hellonote/'
+        : '/',
+    lintOnSave: false,
+    devServer: {
+        https: true
+    },
+    pluginOptions: {
+        pwa: {
+            iconPaths: {
+                favicon32: './favicon.ico',
+                favicon16: './favicon.ico',
+                appleTouchIcon: './favicon.ico',
+                maskIcon: './favicon.ico',
+                msTileImage: './favicon.ico'
+            }
+        },
+    }
 }
